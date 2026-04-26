addb5fba0b7a832b2f68b654af099a10d376cd75
João Victor Fernandes <joaofdias.dev@gmail.com>
2026-04-26 18:29:39 -0300

    feat: estrutura inicial do projeto com Vite + React + TS

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+    return (
+        <div>
+            <h1>A Big Title</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam obcaecati inventore distinctio tempore harum! Commodi, quibusdam. Eligendi corrupti est itaque voluptates ab, ex alias totam porro provident, doloribus magnam!
+            </p>
+
+            <button>Nossa Missão</button>
+        </div>
+    );
+}
