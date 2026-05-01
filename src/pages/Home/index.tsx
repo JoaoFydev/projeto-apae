8d93c4ea15ca04abc85f7991c87e7e591ff41aa3
João Victor Fernandes <joaofdias.dev@gmail.com>
2026-05-01 04:03:30 -0300

    Foi organizado o projeto nas pastas corretas, mas estou com problemas na parte da instalação do shadcn/ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,27 @@
+
+import { Button } from "@/components/ui/button"
+
 export function Home() {
     return (
         <div>
             <h1>A Big Title</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam obcaecati inventore distinctio tempore harum! Commodi, quibusdam. Eligendi corrupti est itaque voluptates ab, ex alias totam porro provident, doloribus magnam!
-            </p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quam ipsam obcaecati inventore distinctio tempore harum! Commodi, quibusdam. 
+                Eligendi corrupti est itaque voluptates ab, ex alias totam porro provident, doloribus magnam!
+                </p>
+            </div>
+            
+    )
+};
 
-            <button>Nossa Missão</button>
-        </div>
-    );
+export function ButtonDemo() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="icon" aria-label="Submit">
+        <ArrowUpIcon />
+      </Button>
+    </div>
+  )
 }
